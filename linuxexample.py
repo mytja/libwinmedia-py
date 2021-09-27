e12f857bc8224dba58e27f02a9adef635f17d279
mytja <mytja@protonmail.com>
2021-09-27 14:17:37 +0200

    Change links in example.py

--- linuxexample.py
+++ linuxexample.py
@@ -6,7 +6,9 @@ player.set_position_callback(lambda position: print(f"{position} ms."))
 media1 = libwinmedia.Media(
     "https://p.scdn.co/mp3-preview/669eef4c25c47eb54c8c0bceee55b94519f3b0c1?cid=774b29d4f13844c495f206cafdad9c86"
 )
-media2 = libwinmedia.Media("test.mp3")
+media2 = libwinmedia.Media(
+    "https://archive.org/download/Kalimba.mp3_377/Kalimba.mp3"
+)
 playlist = libwinmedia.Playlist(media1, media2)
 
 player.open(playlist)
